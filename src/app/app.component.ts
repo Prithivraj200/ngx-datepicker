ba83c5694bc7218cc490d3d516f94cb713186f22
Prithivraj <prethive200@gmail.com>
2019-06-04 17:15:26 +0530

    ng2-datepicker-reworked

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {DatepickerOptions} from '../ng-datepicker/component/ng-datepicker.component';
+import * as enLocale from 'date-fns/locale/en';
+import * as frLocale from 'date-fns/locale/fr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  date: Date;
+  options: DatepickerOptions = {
+    locale: enLocale,
+    minYear: 2016,
+    maxYear: 2020,
+    displayFormat: 'DD/MM/YYYY'
+  };
+  constructor() {
+    this.date = new Date();
+  }
+}
